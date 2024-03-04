6db50242d45b3c77aef04aab7d6d9d2077e2c454
raulgp <raul.garrigos@hotmail.com>
2024-03-04 12:16:24 +0100

    product model

---
+++ models/Product.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value >= 0;
+        },
+        message: "El precio no puede ser negativo",
+      },
+    },
+    store: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Store",
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 1,
+    },
+    images: [String],
+    reviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
