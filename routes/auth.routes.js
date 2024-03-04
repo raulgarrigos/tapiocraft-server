3831582cd0adf6af40f97c49f632aead3ffa36a9
raulgp <raul.garrigos@hotmail.com>
2024-03-04 13:40:35 +0100

    auth routes register and login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,11 @@ const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 
+const MAX_LOGIN_ATTEMPTS = 5;
+const LOCKOUT_DURATION = 3 * 60 * 1000;
+
+let loginAttempts = {};
+
 // POST "/api/auth/register" to create a new user
 router.post("/register", async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -94,5 +99,97 @@ router.post("/register", async (req, res, next) => {
     });
   }
 });
+
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // Check for empty fields
+  if (!username || !password) {
+    res.status(400).json({ errorMessage: "All fields must be filled" });
+    return;
+  }
+
+  try {
+    if (loginAttempts[username]?.attempts >= MAX_LOGIN_ATTEMPTS) {
+      const timeSinceLastAttempt =
+        Date.now() - loginAttempts[username].lastAttempt;
+      if (timeSinceLastAttempt < LOCKOUT_DURATION) {
+        const timeLeft = LOCKOUT_DURATION - timeSinceLastAttempt;
+        return res.status(429).json({
+          errorMessage: `Too many login attempts. Please try again after ${Math.floor(
+            timeLeft / 60000
+          )} minutes and ${((timeLeft % 60000) / 1000).toFixed(0)} seconds.`,
+        });
+      } else {
+        delete loginAttempts[username]; // Reset attempt counter after lockout period
+      }
+    }
+
+    const foundUser = await User.findOne({ username });
+    if (!foundUser) {
+      incrementLoginAttempts(username);
+      const remainingAttempts =
+        MAX_LOGIN_ATTEMPTS - (loginAttempts[username]?.attempts || 0);
+      const errorMessage =
+        remainingAttempts > 0
+          ? `Incorrect username. You have ${remainingAttempts} attempt${
+              remainingAttempts === 1 ? "" : "s"
+            } left.`
+          : `Too many login attempts. Please try again after ${Math.floor(
+              LOCKOUT_DURATION / 60000
+            )} minutes.`;
+      return res.status(400).json({ errorMessage });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, foundUser.password);
+    if (!isPasswordValid) {
+      incrementLoginAttempts(username);
+      const remainingAttempts =
+        MAX_LOGIN_ATTEMPTS - loginAttempts[username].attempts;
+      const attemptsLeftMessage = `You have ${remainingAttempts} attempt${
+        remainingAttempts === 1 ? "" : "s"
+      } left.`;
+      const errorMessage =
+        remainingAttempts > 0
+          ? `Incorrect password. ${attemptsLeftMessage}`
+          : `Too many login attempts. Please try again after ${Math.floor(
+              LOCKOUT_DURATION / 60000
+            )} minutes.`;
+      return res.status(400).json({ errorMessage });
+    }
+
+    const payload = {
+      _id: foundUser._id,
+      username: foundUser.username,
+      email: foundUser.email,
+      role: foundUser.role,
+    };
+
+    const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+      expiresIn: "12h",
+    });
+
+    delete loginAttempts[username];
+
+    res.json({ authToken });
+  } catch (error) {
+    console.error("Error during login or registration:", error);
+    res.status(500).json({
+      errorMessage: "An error occurred while processing the request.",
+    });
+  }
+});
+
+function incrementLoginAttempts(username) {
+  if (!loginAttempts[username]) {
+    loginAttempts[username] = {
+      attempts: 1,
+      lastAttempt: Date.now(),
+    };
+  } else {
+    loginAttempts[username].attempts++;
+    loginAttempts[username].lastAttempt = Date.now();
+  }
+}
 
 module.exports = router;
