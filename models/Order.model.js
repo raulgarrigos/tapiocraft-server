e9f16da02be04ec7d925d8d523875ab90b55c231
raulgp <raul.garrigos@hotmail.com>
2024-03-04 12:27:04 +0100

    order model

---
+++ models/Order.model.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    store: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Store",
+      required: true,
+    },
+    shippingAddress: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered"],
+      default: "pending",
+    },
+    orderPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    trackingInfo: {
+      trackingNumber: {
+        type: String,
+        trim: true,
+      },
+      carrier: {
+        type: String,
+        trim: true,
+      },
+    },
+    additionalNotes: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
