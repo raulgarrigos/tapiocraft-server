e8d17a0edb6a2dbc3724b6660ac57a378740fb44
raulgp <raul.garrigos@hotmail.com>
2024-04-16 13:09:20 +0200

    solved problem with cart model

--- models/Cart.moddel.js
+++ models/Cart.moddel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   items: [
     {
-      product: { type: Schema.Types.ObjectId, ref: "Product" },
+      product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
       quantity: { type: Number, default: 1 },
     },
   ],
