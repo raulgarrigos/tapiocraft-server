a86ac84bce1766e15f08257a96c30c3a463a8b90
raulgp <raul.garrigos@hotmail.com>
2024-04-16 13:03:01 +0200

    Cart model

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,6 +1,17 @@
+const isTokenValid = require("../middlewares/auth.middlewares");
+const Product = require("../models/Product.model");
+
 const router = require("express").Router();
 
 // GET /api/cart to get the information of the current user's cart.
+router.get("/", isTokenValid, async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
+});
 // POST /api/cart/products/:productId to add a product to the cart.
 // DELETE /api/cart/products/:productId to remove a product from the cart.
 
