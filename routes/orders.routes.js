c45b9ae57ba76e809e1f3e120067015a53b68c81
raulgp <raul.garrigos@hotmail.com>
2024-04-23 12:42:14 +0200

    Populate route get specific order

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
+const isTokenValid = require("../middlewares/auth.middlewares");
 const Order = require("../models/Order.model");
 
 // GET /api/orders/userId to get a list of orders placed by the current user.
-router.get("/:userId", async (req, res, next) => {
+router.get("/:userId", isTokenValid, async (req, res, next) => {
   try {
     const orders = await Order.find({ user: req.params.userId });
     res.json(orders);
@@ -12,12 +13,15 @@ router.get("/:userId", async (req, res, next) => {
 });
 
 // GET /api/orders/userId/:orderID to get the information of a specific order.
-router.get("/userId/:orderId", async (req, res, next) => {
+router.get("/:userId/:orderId", isTokenValid, async (req, res, next) => {
   try {
     const response = await Order.findOne({
       user: req.params.userId,
       _id: req.params.orderId,
-    });
+    })
+      .populate("user")
+      .populate("stores")
+      .populate("products.product");
     res.json(response);
   } catch (error) {
     next(error);
