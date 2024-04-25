a3509a041967e9c9a618ce4bae9e86eff168ec0f
raulgp <raul.garrigos@hotmail.com>
2024-04-25 11:54:45 +0200

    Routes for profile picture

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,7 +1,7 @@
-const isTokenValid = require("../middlewares/auth.middlewares");
-const User = require("../models/User.model");
-
 const router = require("express").Router();
+const User = require("../models/User.model");
+const isTokenValid = require("../middlewares/auth.middlewares");
+const uploader = require("../middlewares/cloudinary.config");
 
 // GET /api/profile/:userId to get the current user's profile information.
 router.get("/:userId/details", isTokenValid, async (req, res, next) => {
@@ -32,5 +32,45 @@ router.put("/:userId/update", isTokenValid, async (req, res, next) => {
     next(error);
   }
 });
+
+// POST "/api/profile/image" para acceder a cloudinary y actualizar la imagen de perfil
+router.post(
+  "/image",
+  isTokenValid,
+  uploader.single("image"),
+  (req, res, next) => {
+    if (!req.file) {
+      res
+        .status(400)
+        .json({ errorMessage: "Ha habido un error con la imagen" });
+      return;
+    }
+    res.json({ imageUrl: req.file.path });
+  }
+);
+
+// PATCH "/api/profile/image" para actualizar imagen del perfil
+router.patch(
+  "/image",
+  isTokenValid,
+  uploader.single("image"),
+  async (req, res, next) => {
+    if (!req.file) {
+      res
+        .status(400)
+        .json({ errorMessage: "Ha habido un error con la imagen" });
+      return;
+    }
+
+    try {
+      await User.findByIdAndUpdate(req.payload._id, {
+        profilePicture: req.file.path,
+      });
+      res.json({ imageUrl: req.file.path });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
